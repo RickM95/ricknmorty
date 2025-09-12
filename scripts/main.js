ed429db73bb4b12eecd7a6b62668a9305437fed5
NefiPineda <nefipinedagal@gmail.com>
2025-09-12 12:03:08 -0600

    funcion de las cards en principal

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+import { initializeCards } from './cards.js';
+
+/**
+ * Inicializa la aplicación cuando el DOM está cargado
+ */
+document.addEventListener('DOMContentLoaded', () => {
+  initializeCards();
+});
